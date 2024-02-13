d4104fa44f6a2a2bd7ae22e579539bbc7cc58136
PradnyaSJ <dokepradnya56@gmail.com>
2024-02-13 09:24:54 +0530

    Crud Array

---
+++ CrudArray/src/com/crudarray/BookDetails.java
@@ -0,0 +1,13 @@
+package com.crudarray;
+
+public interface BookDetails {
+		
+	void addBook();
+	void updateBook();
+	void deleteBook();
+	void searchBookById();
+	void searchBookByName();
+	void searchBookByAuthor();
+	void showAllBook();
+	
+}
